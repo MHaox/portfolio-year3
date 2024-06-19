e3d1215dac93a4ad529fdac9a0c0f1d42ce8afa7
MAX HOOGEWEG <mr.hoogeweg@student.alfa-college.nl>
2024-06-19 16:48:33 +0200

    Update logo and navbar styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,55 +1,57 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import git from "../assets/img/github-mark-white.svg"
-import linked from "../assets/img/linkedin-mark-white.svg"
-import insta from "../assets/img/instagram-mark-white.svg"
-import logo from "../assets/img/logo.svg"
+import git from "../assets/img/github-mark-white.svg";
+import linked from "../assets/img/linkedin-mark-white.svg";
+import insta from "../assets/img/instagram-mark-white.svg";
+import logo from "../assets/img/logo.svg";
 
 export const NavBar = () => {
-    const[activeLink, setActiveLink] = useState('home');
-    const[scrolled, setScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() =>{
-        const onScroll = () =>{
-            if(window.scrollY > 50){
-                setScrolled(true);
-            }else{
-                setScrolled(false);
-            }
-        }
-        window.addEventListener('scroll', onScroll);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-        return () => window.removeEventListener('scroll', onScroll);
-    },    [])
+    window.addEventListener("scroll", onScroll);
 
-    const onUpdateActivelink = (value) => {
-        setActiveLink(value)
-    }
-    return (
-        <Navbar expand="lg" className={scrolled ? 'scrolled': ''}>
-            <Container>
-                <Navbar.Brand href="#home">
-                    <img src={logo} alt="Logo" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav">
-                    <span className="navbar-toggler-icon"></span>
-                </Navbar.Toggle>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbarlink'} onClick={() => onUpdateActivelink('home')}>Home</Nav.Link>
-                        <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbarlink'} onClick={() => onUpdateActivelink('skills')}>Skills</Nav.Link>
-                        <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbarlink'} onClick={() => onUpdateActivelink('projects')}>Projects</Nav.Link>
-                    </Nav>
-                    <span className="navbar-text">
-                        <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/max-robert-hoogeweg/"><img src={linked} alt="LinkedIn" /></a>
-                            <a href="https://github.com/MaxHoogeweg"><img src={git} alt="GitHub" /></a>
-                            <a href="https://www.instagram.com/mhaox.global"><img src={insta} alt="Insagram" /></a>
-                        </div>
-                        <div className="vvd" onClick={() => console.log('Connect')}><span>Let's Connect</span></div>
-                    </span>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
-}
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/max-robert-hoogeweg/"><img src={linked} alt="LinkedIn" /></a>
+              <a href="https://github.com/MaxHoogeweg"><img src={git} alt="GitHub" /></a>
+              <a href="https://www.instagram.com/mhaox.global"><img src={insta} alt="Insagram" /></a>
+            </div>
+            <button className="vvd"><span>Let’s Connect</span></button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
